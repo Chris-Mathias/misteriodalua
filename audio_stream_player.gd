886de16483e083a8069fe12a721599ce5a591069
Leoccker <leonardo.becker@sou.unijui.edu.br>
2025-06-24 14:56:42 -0300

    trilha sonora menu e fase1

---
+++ audio_stream_player.gd
@@ -0,0 +1,42 @@
+extends AudioStreamPlayer
+
+var music_tracks = {
+	"menu": {
+		"track": preload("res://assets/trilha_sonora/menu.mp3"),
+		"volume": -5.0
+		},
+	"fase1": { 
+		"track": preload("res://assets/trilha_sonora/fase1.mp3"),
+		"volume": -10.0
+		}
+}
+
+var musica_atual = null
+
+func tocar_musica(nome_da_faixa):
+	if not music_tracks.has(nome_da_faixa):
+		print("música não existente")
+		return
+	
+	if musica_atual == nome_da_faixa:
+		return
+	
+	var info_da_faixa = music_tracks[nome_da_faixa]
+	
+	musica_atual = nome_da_faixa
+	
+	stream = info_da_faixa["track"]
+	volume_db = info_da_faixa["volume"]
+	
+	play()
+	
+	print("musica iniciada")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
